88871313d608c0b529651ad5cc4a59a314904414
nzeiner <nikolaus.zeiner@gmail.com>
2021-02-26 22:15:01 +0100

    Cookie Banner

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+if(!empty($_POST["submit"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["subject"];
+	$content = $_POST["message"];
+
+	$toEmail = "nikolaus.zeiner@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
+?>
